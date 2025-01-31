7bb028d8690ade8164da61581b48ae53f46f0f1a
Kefilino Khalifa Filardi <kefilino@live.com>
2025-01-31 17:30:01 +0800

    Add header navigation component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import HeaderNav from "@/components/header-nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +34,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <HeaderNav/>
+        <div className="h-px min-h-[calc(100vh-4rem)]">
+          {children}
+        </div>
       </body>
     </html>
   );
